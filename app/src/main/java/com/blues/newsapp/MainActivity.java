19ce7c1e779ed52b70ab57a321a1f8cf2fb6726d
PoBlue <250299430@qq.com>
2017-12-07 22:54:19 +0800

    [setting] news setting screen

--- app/src/main/java/com/blues/newsapp/MainActivity.java
+++ app/src/main/java/com/blues/newsapp/MainActivity.java
@@ -9,6 +9,9 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -117,5 +120,28 @@ public class MainActivity extends AppCompatActivity
         mAdapter.clear();
     }
 
+    /**
+     * Methods for setting up the menu
+     **/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
+        MenuInflater inflater = getMenuInflater();
+        /* Use the inflater's inflate method to inflate our visualizer_menu layout to this menu */
+        inflater.inflate(R.menu.news_menu, menu);
+        /* Return true so that the visualizer_menu is displayed in the Toolbar */
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingsActivity);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
